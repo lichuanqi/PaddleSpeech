21248af7d347c7041863b4bc8e82597e7aa2453c
lichuanqi <lc@dlc618.com>
2022-12-05 20:03:42 +0800

    修改：使用datetime模块处理时间

--- deploy/speech_client_gui/client_asr_gui.py
+++ deploy/speech_client_gui/client_asr_gui.py
@@ -1,10 +1,9 @@
 import os
 import sys
-import time
 import json
+from datetime import datetime
 import base64
 import requests
-
 import pyaudio
 from winsound import PlaySound
 import wave
@@ -12,7 +11,7 @@ import wave
 import threading
 import tkinter
 
-path = os.getcwd()
+path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(path)
 from utils import mk_if_not_exits
 
@@ -78,12 +77,6 @@ class Recorder:
         # 主窗口循环
         window.mainloop()
 
-    def get_current_time(self):
-        """
-        获取当前时间
-        """
-        current_time = time.strftime('%H:%M:%S',time.localtime(time.time()))
-        return current_time
 
     def start(self):
         """
@@ -102,12 +95,12 @@ class Recorder:
             self.result_text.insert('end', err)
 
         else:
-            msg = f'识别到条码号: {post_id}\n'
+            self.post_id = post_id
+            msg = f'识别到条码号: {self.post_id}\n'
             self.result_text.insert('end', msg)
-            self.post_id_checked = post_id
-
+            
             self._running = True
-            msg = self.get_current_time() + " 开始录音...\n"
+            msg = get_time() + " 开始录音...\n"
             self.result_text.insert('end', msg)
 
             self._frames = []
@@ -133,7 +126,7 @@ class Recorder:
         停止录音,将录音文件保存至本地指定路径
         """
         # 保存路径及名称
-        savename = os.path.join(self.path_wavs, f'{self.post_id_checked}.wav')
+        savename = os.path.join(self.path_wavs, f'{self.post_id}.wav')
 
         if self._running:
             # 录音结束
@@ -148,11 +141,11 @@ class Recorder:
             wf.writeframes(b''.join(self._frames))
             wf.close()
             self.previous_path = savename
-            msg = self.get_current_time() + f" 录音结束,保存至 {savename}\n"
+            msg = get_time() + f" 录音结束,保存至 {savename}\n"
             self.result_text.insert('end', msg)
 
         else:
-            msg = self.get_current_time() + ' 请先开始录音\n'
+            msg = get_time() + ' 请先开始录音\n'
             self.result_text.insert('end', msg)
 
 
@@ -162,9 +155,9 @@ class Recorder:
         """
         if self.previous_path:
             PlaySound(self.previous_path, flags=1)
-            msg = self.get_current_time() + " 正在播放文件: {}\n".format(self.previous_path)
+            msg = get_time() + " 正在播放文件: {}\n".format(self.previous_path)
         else:
-            msg = self.get_current_time() + " 请先录音并保存"
+            msg = get_time() + " 请先录音并保存"
         self.result_text.insert('end', msg)
 
     def http_predict_thread(self):
@@ -177,85 +170,91 @@ class Recorder:
         """
         根据文件保存路径使用http接口进行语音识别
         """
-        # 检查邮件条码号是否符合要求
-        post_id = self.e1.get()
-        res, err = check_post_id(str(post_id))
-        if not res:
-            self.result_text.insert('end', err)
+        server_ip="192.168.35.221"
+        # server_ip="127.0.0.1"
+        port="8888"
+        sample_rate=16000
+        lang="zh_cn"
+        audio_format="wav"
+        endpoint="/paddlespeech/asr"
 
-        else:
-            server_ip="192.168.35.221"
-            # server_ip="127.0.0.1"
-            port="8888"
-            sample_rate=16000
-            lang="zh_cn"
-            audio_format="wav"
-            endpoint="/paddlespeech/asr"
-
-            input = self.previous_path
-            msg = self.get_current_time() + " 开始预测语音文件：%s\n" % (input)
-            self.result_text.insert('end', msg)
-            
-            # 音频文件编码
-            with open(input, 'rb') as f:
-                base64_bytes = base64.b64encode(f.read())
-                audio = base64_bytes.decode('utf-8')
-            
-            # 拼接api
-            if server_ip is None or port is None:
-                server_url = None
-            else:
-                server_url = 'http://' + server_ip + ":" + str(port) + endpoint
-            msg = self.get_current_time() + f" 调用接口: {server_url}\n"
-            self.result_text.insert('end', msg)
-
-            data = {
-                "audio": audio,
-                "audio_format": audio_format,
-                "sample_rate": sample_rate,
-                "lang": lang,
-            }
-            
-            try:
-                # 调用接口
-                re_ts = time.time()
-                response = requests.post(server_url, data=json.dumps(data))
-                data = json.loads(response.text)
-                re_tn = time.time()
-                msg = self.get_current_time() + " 接口响应时间: %.2f s.\n" % (re_tn - re_ts)
-                self.result_text.insert('end', msg)
-
-                res = data['result']['transcription']
-                msg = self.get_current_time() +  f" 语音识别结果: {res}\n"
-                self.result_text.insert('end', msg)
-
-                # 结果保存至txt
-                self.result_to_txt(post_id, res)
-
-            # 无法连接接口
-            except requests.exceptions.ConnectionError as err:
-                msg = self.get_current_time()  + str(err) + '\n'
-                self.result_text.insert('end', msg)
-
-
-    def result_to_txt(self, id, result):
-        """把识别结果保存为txt文件
-        """
-        savename = os.path.join(self.path_results, f'{self.post_id_checked}.txt')
-        with open(savename, 'w', encoding='utf-8') as f:
-            f.write(id + ' ' + result)
-        
-        msg = self.get_current_time() + f'结果已保存至: {savename}'
+        input = self.previous_path
+        msg = get_time() + " 开始预测语音文件：%s\n" % (input)
         self.result_text.insert('end', msg)
+        
+        # 音频文件编码
+        with open(input, 'rb') as f:
+            base64_bytes = base64.b64encode(f.read())
+            audio = base64_bytes.decode('utf-8')
+        
+        # 拼接api
+        if server_ip is None or port is None:
+            server_url = None
+        else:
+            server_url = 'http://' + server_ip + ":" + str(port) + endpoint
+        msg = get_time() + f" 调用接口: {server_url}\n"
+        self.result_text.insert('end', msg)
+
+        data = {
+            "audio": audio,
+            "audio_format": audio_format,
+            "sample_rate": sample_rate,
+            "lang": lang,
+        }
+        
+        try:
+            # 调用接口
+            re_ts = datetime.now()
+            response = requests.post(server_url, data=json.dumps(data))
+            data = json.loads(response.text)
+            re_tn = datetime.now()
+            msg = get_time() + " 接口响应时间: %.2f s.\n" % (re_tn - re_ts)
+            self.result_text.insert('end', msg)
+
+            res = data['result']['transcription']
+            msg = get_time() +  f" 语音识别结果: {res}\n"
+            self.result_text.insert('end', msg)
+
+            # 结果保存至txt
+            
+            result_to_txt(self.path_results, self.post_id, res)
+            msg = get_time() + f'结果已保存至: {self.path_results}/{self.post_id}.txt'
+            self.result_text.insert('end', msg)
+
+        # 无法连接接口
+        except requests.exceptions.ConnectionError as err:
+            msg = get_time()  + str(err) + '\n'
+            self.result_text.insert('end', msg)
+
+
+def result_to_txt(self, savapath, post_id, result):
+    """把语音识别结果保存为txt文件
+
+    Params
+        savapath
+        post_id
+        result
+    Output
+        savapath/post_id.txt
+    """
+    savename = os.path.join(savapath, f'{post_id}.txt')
+    with open(savename, 'w', encoding='utf-8') as f:
+        f.write(post_id + ',' + result)
+        
+
+def get_time():
+    """获取字符串格式的当前时间
+    """
+    return datetime.now().strftime(r'%H:%M:%S')
 
 
 def check_post_id(post_id: str):
     """检查post_id是否符合要求
 
     检查顺序:
-        (1)是否为默认空值
-        (2)是否为纯数字
-        (3) TODO: 长度是否符合
+        1.是否为默认空值
+        2.是否为纯数字
+        3.TODO: 长度是否符合
 
     Params
         post_id (str)
