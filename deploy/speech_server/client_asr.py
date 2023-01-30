6aa8b4c99851f68d9a73a47b2a7a347fa1e070fa
lichuanqi <lc@dlc618.com>
2023-01-30 10:01:01 +0800

    修改 - asr客户端调用接口时分为使用cli高级接口和requests底层接口两种

--- deploy/speech_server/client_asr.py
+++ deploy/speech_server/client_asr.py
@@ -6,8 +6,8 @@ import base64
 
 import requests
 
-# from paddlespeech.cli.log import logger
-# from paddlespeech.server.bin.paddlespeech_client import ASRClientExecutor
+from paddlespeech.cli.log import logger
+from paddlespeech.server.bin.paddlespeech_client import ASRClientExecutor
 
 
 def wav2base64(wav_file: str):
@@ -19,60 +19,57 @@ def wav2base64(wav_file: str):
         base64_string = base64_bytes.decode('utf-8')
     return base64_string
 
-# def client_cli():
-#     """
-#     使用cli的高级接口预测
-#     """
-#     asrclient_executor = ASRClientExecutor()
-#     res = asrclient_executor(
-#         input="/home/xxtc/lichuan/Dataset/speech/002.wav",
-#         server_ip="127.0.0.1",
-#         port=8090,
-#         sample_rate=16000,
-#         lang="zh_cn",
-#         audio_format="wav")
 
-#     print(res)
+def client_http_by_cli(input_wav):
+    """使用cli的高级接口预测
+    
+    Params
+        input: 音频文件
+
+    Output
+        res: 预测结果
+    """
+    asrclient_executor = ASRClientExecutor()
+    res = asrclient_executor(
+        input=input_wav,
+        server_ip="192.168.35.221",
+        port=8888,
+        sample_rate=16000,
+        lang="zh_cn",
+        audio_format="wav")
+
+    print(res)
 
 
-if __name__=='__main__':
+def client_http_by_request(input_wav):
 
-    input="zh.wav"
-    server_ip="192.168.35.221"
-    port="8888"
+    API_ASR_HTTP = 'http://192.168.35.221:8888/paddlespeech/asr'
+    
     sample_rate=16000
     lang="zh_cn"
     audio_format="wav"
-    endpoint="/paddlespeech/asr"
-    protocol = "http"
-
+    
     time_start = time.time()
+    print(f"asr http client start, endpoint: {API_ASR_HTTP}")
 
-    if protocol.lower() == "http":
-        
-        if server_ip is None or port is None:
-            server_url = None
-        else:
-            server_url = 'http://' + server_ip + ":" + str(port) + endpoint
-        print(f"asr http client start, endpoint: {server_url}")
+    audio = wav2base64(input_wav)
+    data = {
+        "audio": audio,
+        "audio_format": audio_format,
+        "sample_rate": sample_rate,
+        "lang": lang,
+    }
 
-        audio = wav2base64(input)
-        data = {
-            "audio": audio,
-            "audio_format": audio_format,
-            "sample_rate": sample_rate,
-            "lang": lang,
-        }
-
-        res = requests.post(server_url, data=json.dumps(data))
-        res = res.json()['result']['transcription']
-        
-    else:
-        print(f"Sorry, we have not support protocol: {protocol},"
-                        "please use http or websocket protocol")
-        sys.exit(-1)
+    res = requests.post(API_ASR_HTTP, data=json.dumps(data))
+    res = res.json()['result']['transcription']
 
     time_end = time.time()
-    print("asr http client finished")
-    print(f"ASR result: {res}")
-    print("Response time %f s." % (time_end - time_start))
+    print(f"ASR http result: {res}")
+    print("ASR http response time %f s." % (time_end - time_start))
+
+
+if __name__=='__main__':
+    input_wav = "zh.wav"
+
+    # client_http_by_cli(input_wav)
+    client_http_by_request(input_wav)
